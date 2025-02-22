9496bd7f26a5e7049111c337b631cc856ab6168a
Harshil <ghagrawal17@gmail.com>
2025-02-22 02:05:07 +0100

    to prod

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
 import type { Config } from "drizzle-kit";
+// import { config } from "dotenv";
 
+// if (process.env.ENVIRONMENT === "production") {
+//   config({ path: '.dev.vars' })
+// }
 export default {
   out: "./drizzle",
   schema: "./database/schema.ts",
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    databaseId: "your-database-id",
+    databaseId: "0cf03998-30a5-4a7f-a496-42ea5051a36c",
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
     token: process.env.CLOUDFLARE_TOKEN!,
   },
